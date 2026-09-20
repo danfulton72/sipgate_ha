dd8fd764fa25705a7e24f19bb77415c84474055d
danfulton72 <42965092+danfulton72@users.noreply.github.com>
2026-09-20 22:47:07 +0100

    Add history refresh option constants

--- custom_components/sipgate_ha/const.py
+++ custom_components/sipgate_ha/const.py
@@ -12,10 +12,12 @@ CONF_PUBLIC_URL: Final = "public_url"
 CONF_CONTACTS: Final = "contacts"
 CONF_INCLUDE_OUTGOING: Final = "include_outgoing"
 CONF_SIGNIFICANT_DIGITS: Final = "significant_digits"
+CONF_HISTORY_REFRESH_MINUTES: Final = "history_refresh_minutes"
 
 DEFAULT_INCLUDE_OUTGOING: Final = False
 DEFAULT_SIGNIFICANT_DIGITS: Final = 9
 DEFAULT_HISTORY_LIMIT: Final = 10
+DEFAULT_HISTORY_REFRESH_MINUTES: Final = 0
 
 EVENT_CALL_STARTED: Final = "sipgate_call_started"
 EVENT_CALL_ANSWERED: Final = "sipgate_call_answered"
